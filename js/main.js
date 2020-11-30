6fcb46b67b8237f8036e48454a45313864538935
Simona Cinti <simona.cinti2@gmail.com>
2020-11-30 16:01:39 +0100

    first setup vue menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,34 @@ console.log('hello', Vue);
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        sections: [
+            {
+                name: 'Home',
+                // link: 'https://www.google.com/'
+
+            },
+            {
+                name: 'Shop',
+                link: 'https://www.google.com/'
+            },
+            {
+                name: 'About',
+                link: 'https://www.google.com/'
+            },
+            {
+                name: 'Blog',
+                link: 'https://www.google.com/'
+            },
+            {
+                name: 'Contact',
+                link: 'https://www.google.com/'
+            },
+            {
+                name: 'Shop by brand',
+                link: 'https://www.google.com/'
+            },
+
+        ]
     }
+    
 })
